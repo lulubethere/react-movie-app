72218601439d055807d30709ff9b792926c74822
lulubethere <cuz2463@gmail.com>
2024-12-10 13:47:46 +0900

    practice react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
+function App() {
+  return <Router>
+    <div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/movie/:id" component={Detail} />
+      </Switch>
+    </div>
+  </Router>
+}
+
+export default App;
